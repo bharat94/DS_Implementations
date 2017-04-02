e036592a45034bcaf7e2848cff599ebd690ac980
Bharat V <vaidhyanathan.b@husky.neu.edu>
2017-04-02 19:03:21 -0400

    iStack accessor and provider methods push

---
+++ iStack.java
@@ -0,0 +1,16 @@
+// A custom implementation of the stack data structure
+
+//currently supports only integers, will soon be ported to support generics
+
+//interface:
+/*
+	iStack() : creates an empty stack
+	iStack(List <Integer>)
+	iStack(int[])
+
+	push()
+	pop()
+	size()
+	invert()
+
+*/
